330152d07da31fd83ab238d4eb684ff2ebb33773
Emre <esrn@outlook.de>
2022-09-08 21:35:27 +0200

    Fehlerbehebung

--- rechteckgenerator_staticlib.c
+++ rechteckgenerator_staticlib.c
@@ -1,13 +1,14 @@
 #include<stdio.h>
-#include<rechteckgenerator_staticlib.h>
 
-void generator(int amp, int frequenz){
-	int i;
-	while(i<(frequenz/2)){
-		printf("%s",amp);
-	}
-	while(i<frequenz){
-		printf("- %s",amp);
-	}
+void generator(int amp, int frequenz, int zp, FILE *output_stream) {
+
+    while(zp<(frequenz/2)) {
+        fprintf(output_stream, "%ds:%dV \n",zp,amp);
+        zp++;
+    }
+    while(zp<frequenz) {
+        fprintf(output_stream, "%ds: -%dV \n",zp,amp);
+        zp++;
+    }
 }
 
