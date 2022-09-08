6d2b1ae92ef6f0352fbf8f3413d77ed77cce04ed
Emre <esrn@outlook.de>
2022-09-08 21:36:03 +0200

    Fehlerbehebung

--- main.c
+++ main.c
@@ -6,41 +6,38 @@
 void help() {
     printf("Bitte nutzen Sie folgende Argumente \n");
     printf("-h: Hilfe \n");
-    printf("-e: Angabe des Erwartungswerts (ganzzahlig) \n");
-    printf("-s: Angabe der Standardabweichung (ganzzahlig) \n");
-    printf("-a: Angabe der Anzahl an Werten die generiert werden sollen (ganzzahlig) \n");
+    printf("-e: Eingabe der Werte \n");
 }
 
 int main(int argc, char* argv[]) {
 
-	int opt;
-	int amp;
-	int frequenz;
-	int ap;
+    int opt;
+    int amp;
+    int frequenz;
+    int ap;
 
-	while((opt=getopt(argc, argv, "h:e:"))!=-1) {
-		switch(opt) {
-			case 'h':
-				help();
-				return 0;
-				break;
-			case 'e':
-				printf("Amplitude (Ganzzahl):");
-				scanf("%i",&amp);
-				printf("Frequenz (in Hz, Ganzzahl):");
-				scanf("%i",&frequenz);
-				printf("Amplitude (Ganzzahl):");
-				printf("Periodenanzahl (Ganzzahl):");
-				scanf("%i",&ap);
-				break;
-		}
-	}
+    while((opt=getopt(argc, argv, "he"))!=-1) {
+        switch(opt) {
+        case 'h':
+            help();
+            return 0;
+            break;
+        case 'e':
+            printf("Amplitude (Ganzzahl):");
+            scanf("%i",&amp);
+            printf("Frequenz (in Hz, Ganzzahl):");
+            scanf("%i",&frequenz);
+            printf("Periodenanzahl (Ganzzahl):");
+            scanf("%i",&ap);
+            break;
+        }
+    }
 
-	FILE *output_stream;
-	output_stream = fopen("output_stream.txt","w");
-	for(int zp=0;i<ap;i++) {
-		generator(amp,frequenz,zp,output_stream.txt);
-	}
-	fclose(output_stream);
-	return 0;
+    FILE *output_stream;
+    output_stream = fopen("output_stream.txt","w");
+    for(int zp=0; zp<ap; zp++) {
+        generator(amp,frequenz,zp,output_stream);
+    }
+    fclose(output_stream);
+    return 0;
 }
